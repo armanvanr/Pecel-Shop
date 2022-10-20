7663abb020cf7ef656decabd05728ba7a9b45c0a
Venska Arman Nur Rosyidin <armanvenska@gmail.com>
2022-10-20 13:11:52 +0700

    Tambahan arman
    notes, create order

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def home():
 @app.route("/pecel", methods=["POST"])
 def pecel_post():
     pecel_receive = request.form['pecel_give']
-
+    notes_receive = request.form['notes_give']
     count = db.pecel.count_documents({})
     num = count + 1
 
@@ -22,7 +22,9 @@ def pecel_post():
         'num': num,
         'pecel': pecel_receive,
         'done': 0,
-        'qty': 1
+        'qty': 1,
+        'notes': notes_receive
+        
     }
     db.pecel.insert_one(doc)
     return jsonify()
@@ -50,6 +52,10 @@ def delete_pecel():
     num_receive = request.form['num_give']
     db.pecel.delete_one({'num': int(num_receive)})
     return jsonify()
+
+@app.route("/pecel/create", methods=["POST"])
+def order_create():
+    return jsonify({'msg': 'Order created! Total bills: IDR [number]'})
 
 @app.route("/pecel", methods=["GET"])
 def pecel_get():
